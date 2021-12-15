ea47ead773ba62ce36ba9846eb58eedf1e0d00d7
Varanda <m@varanda.ca>
2021-12-15 10:57:10 -0500

    attempt to add makefile extension.

---
+++ libffwplayer/ffwplayer.h
@@ -0,0 +1,35 @@
+
+/******************************************
+ * 
+ * Simple messaging system across threads
+ * 
+ * License: GPL-3
+ * Copyrights: Marcelo Varanda
+ * 
+ ******************************************/
+
+#pragma once
+
+/**
+ * @file
+ * @brief ffwplayer
+ * 
+ *
+ */
+
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/**
+ * @brief internal function.
+ * @warning App should NOT call this function 
+ */
+void ffwplayer_init(void);
+
+
+
+#ifdef __cplusplus
+  }
+#endif
