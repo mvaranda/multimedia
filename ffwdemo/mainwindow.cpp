f15c7858d7eb396c6a3aadf3afb8d48c9b9dd97a
Varanda <m@varanda.ca>
2021-12-17 10:59:49 -0500

    reformat player row 0.

--- ffwdemo/mainwindow.cpp
+++ ffwdemo/mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
   ui->setupUi(this);
   videoCells[0].video_area = ui->lb_video_area_0;
-  videoCells[0].t_url = ui->t_url;
+  videoCells[0].t_url = ui->t_url_0;
   videoCells[0].bt_file = ui->bt_select_file_0;
 
   // pre populate
