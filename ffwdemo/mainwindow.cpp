2336878d701f5f34fde8d1a3d3fcd1bbd4b56762
Varanda <m@varanda.ca>
2021-12-17 09:41:29 -0500

    WIP init ui.

--- ffwdemo/mainwindow.cpp
+++ ffwdemo/mainwindow.cpp
@@ -2,14 +2,24 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  videoCells[0].video_area = ui->lb_video_area_0;
+  videoCells[0].t_url = ui->t_url;
+  videoCells[0].bt_file = ui->bt_select_file_0;
+
+  // pre populate
+  videoCells[0].t_url->setPlainText("~/Videos/ironman.mp4");
+  QImage image("imgPath");
+  videoCells[0].video_area->setPixmap(QPixmap::fromImage(image));
+
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
+
 
--- ffwdemo/mainwindow.h
+++ ffwdemo/mainwindow.h
@@ -2,20 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPlainTextEdit>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+#define NUM_VIDEO_CELLS 6
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    class VideoCell{
+    public:
+        QLabel * video_area;
+        QPlainTextEdit * t_url;
+        QPushButton * bt_file;
+    };
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QLabel * video_area_0 = nullptr;
+    QPlainTextEdit * t_url_0 = nullptr;
+    QPushButton * bt_file = nullptr;
+
+    VideoCell videoCells[NUM_VIDEO_CELLS];
 };
 #endif // MAINWINDOW_H
